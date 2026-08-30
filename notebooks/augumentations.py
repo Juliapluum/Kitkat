5671fcac406b1fe253d7726239cfe79d64ee3027
Juliapluum <97413562+Juliapluum@users.noreply.github.com>
2026-08-31 01:41:56 +0200

    f
    f

--- notebooks/augumentations.py
+++ notebooks/augumentations.py
@@ -1,107 +1,34 @@
-import torch
+from PIL import Image
+import numpy as np
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
-import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
 
-import random
+class AlbumentationsImageFolder(ImageFolder):
 
+    def __init__(self,root,transform=None):
+        super().__init__(root=root,transform=None)
 
-
-
-class RandomDarkness:
-
-    def __init__(self, p=0.5, min_factor=0.55, max_factor=0.80):
-        self.p = p
-        self.min_factor = min_factor
-        self.max_factor = max_factor
-
-    def __call__(self, img):
-
-        if random.random() > self.p:
-            return img
-
-        factor = random.uniform(
-            self.min_factor,
-            self.max_factor
-        )
-
-        return transforms.functional.adjust_brightness(
-            img,
-            factor
-        )
+        self.albumentations_transform = transform
 
 
-class RandomNight:
+    def __getitem__(self, index):
 
-    def __init__(
-        self,
-        p=0.35,
-        brightness_min=0.20,
-        brightness_max=0.40
-    ):
-        self.p = p
-        self.brightness_min = brightness_min
-        self.brightness_max = brightness_max
+        path, target = self.samples[index]
 
-    def __call__(self, img):
+        image = Image.open(path).convert("RGB")
 
-        if random.random() > self.p:
-            return img
+        image = np.array(image)
 
-        brightness = random.uniform(
-            self.brightness_min,
-            self.brightness_max
-        )
 
-        img = transforms.functional.adjust_brightness(
-            img,
-            brightness
-        )
+        if self.albumentations_transform is not None:
 
-        tensor = transforms.functional.pil_to_tensor(img).float() / 255.0
+            augmented = self.albumentations_transform(
+                image=image
+            )
 
-        tensor[2] = torch.clamp(
-            tensor[2] + 0.08,
-            0.0,
-            1.0
-        )
+            image = augmented["image"]
 
-        return transforms.functional.to_pil_image(tensor)
 
-class RandomBlur:
-
-    def __init__(
-        self,
-        p=0.45,
-        kernel_min=3,
-        kernel_max=9
-    ):
-        self.p = p
-        self.kernel_min = kernel_min
-        self.kernel_max = kernel_max
-
-    def __call__(self, img):
-
-        if random.random() > self.p:
-            return img
-
-        kernel_size = random.choice(
-            [3, 5, 7, 9]
-        )
-
-        sigma = random.uniform(
-            0.5,
-            2.0
-        )
-
-        return transforms.functional.gaussian_blur(
-            img,
-            kernel_size=kernel_size,
-            sigma=sigma
-        )
-
-random_occlusion = transforms.RandomErasing(
-    p=0.30,
-    scale=(0.05, 0.30),
-    ratio=(0.5, 2.0),
-    value=0
-)
+        return image, target
